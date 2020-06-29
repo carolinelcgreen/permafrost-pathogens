2bbc82a62f13fa7e75d8bcfc2a33325e2667de21
carolinelcgreen <caroline@Carolines-MacBook-Pro-3.local>
2020-06-29 17:18:59 -0400

    upload sample species lists
    sample tables contain different lists of species that have a significant intersection

--- speciesComparison.R
+++ speciesComparison.R
@@ -1,1 +1,23 @@
 # intersect code between two lists of species
+
+# install reading packages
+install.packages("readxl")
+library("readxl")
+
+# install.packages("dplyr")
+
+
+# upload xlxs file
+sampleA <- read_excel("sampleA_table.xlsx")
+sampleB <- read_excel("sampleB_table.xlsx")
+
+# test for individual species first
+sp1 <- "Helicobacter pylori"
+sp2 <- "Campylobacter jejuni"
+sp3 <- "Helicobacter coli"
+grepl(sp1, sp2, fixed = TRUE)
+grepl(sp1, sp3, fixed = TRUE)
+grepl(sp3, sp1, fixed = TRUE)
+
+
+                       
